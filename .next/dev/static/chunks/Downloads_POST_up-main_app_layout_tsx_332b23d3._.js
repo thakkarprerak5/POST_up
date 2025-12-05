c374c37375562e9dfd7f304f585b9c5370545af9
Dhruv Radadiya <dhruvradadiya125@gmail.com>
2025-12-06 02:19:21 +0530

    Fix .gitignore and remove node_modules

--- .next/dev/static/chunks/Downloads_POST_up-main_app_layout_tsx_332b23d3._.js
+++ .next/dev/static/chunks/Downloads_POST_up-main_app_layout_tsx_332b23d3._.js
@@ -2,7 +2,7 @@
     script: typeof document === "object" ? document.currentScript : undefined,
     chunks: [
   "static/chunks/[root-of-the-server]__23d6827c._.css",
-  "static/chunks/9279b_1db20da6._.js",
+  "static/chunks/9279b_c2da45b1._.js",
   "static/chunks/Downloads_POST_up-main_components_0dfb6aef._.js"
 ],
     source: "dynamic"
